5fd1866a44ba22a6355284f8490f05202333e374
Adam Ophoff <aophof46@gmail.com>
2018-08-04 14:30:46 -0400

    fixed some missed parameters

--- wti-reboot.sh
+++ wti-reboot.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 {
 sleep 2
-echo -e "$1\r\n"
+echo -e "$2\r\n"
 sleep 2
-echo -e "/Off $2\r\n"
+echo -e "/Off $3\r\n"
 sleep 1
 echo -e "Y\r\n"
 sleep 4
-echo -e "/On 7\r\n"
+echo -e "/On $3\r\n"
 sleep 1
 echo -e "Y\r\n"
 sleep 2
@@ -15,4 +15,4 @@ echo -e "/X\r\n"
 sleep 2
 echo -e "Y\r\n"
 sleep 2
-} | telnet 192.168.1.3
+} | telnet $1
